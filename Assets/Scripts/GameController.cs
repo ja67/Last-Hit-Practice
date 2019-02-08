4292443d9d4f5e7b0445d78642f022ba3de8364e
Gary <ja67ja67@gmail.com>
2019-02-08 23:13:00 +0800

    Add i18n support

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,138 +4,144 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour
+
+namespace LastHitPractice
 {
-
-    [HideInInspector] public Camera cam;
-    public SoundManager SoundManager;
-    public GameObject infantry;
-    public static GameController instance;
-
-    [HideInInspector] public float timeLeft;
-    [HideInInspector] public Text timerText;
-    [HideInInspector] public int lastHitCount;
-    [HideInInspector] public Text LastHitText;
-
-    public Player player;
-    [HideInInspector] public List<GameObject> friendlyObjectList;
-    [HideInInspector] public List<GameObject> enemyObjectList;
-
-    private const int numOfSpawnCreep = 3;
-
-
-    // Use this for initialization
-
-    private void Awake()
+    public class GameController : MonoBehaviour
     {
-        //Check if there is already an instance of SoundManager
-        if (instance == null)
-            //if not, set it to this.
-            instance = this;
-        //If instance already exists:
-        else if (instance != this)
-            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-            Destroy(gameObject);
 
-        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-        DontDestroyOnLoad(gameObject);
-    }
-    void Start()
-    {
-        if (cam == null)
+        [HideInInspector] public Camera cam;
+        [HideInInspector] public SoundManager SoundManager;
+        public GameObject infantry;
+        public static GameController instance;
+
+        [HideInInspector] public float timeLeft;
+        [HideInInspector] public Text timerText;
+        [HideInInspector] public int lastHitCount;
+        [HideInInspector] public Text LastHitText;
+
+        public Player player;
+        [HideInInspector] public List<GameObject> friendlyObjectList;
+        [HideInInspector] public List<GameObject> enemyObjectList;
+        [HideInInspector] public I18n i18n;
+
+
+        private const int numOfSpawnCreep = 3;
+
+
+        // Use this for initialization
+
+        private void Awake()
         {
-            cam = Camera.main;
+            //Check if there is already an instance of SoundManager
+            if (instance == null)
+                //if not, set it to this.
+                instance = this;
+            //If instance already exists:
+            else if (instance != this)
+                //Destroy this, this enforces our singleton pattern so there can only be one instance of GameController.
+                Destroy(gameObject);
+
+            //Set GameController to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+            DontDestroyOnLoad(gameObject);
         }
-        friendlyObjectList = new List<GameObject>();
-        enemyObjectList = new List<GameObject>();
-
-        StartCoroutine(Spawn());
-        UpdateText();
-    }
-
-    // Update is called once physics timestamp
-    private void FixedUpdate()
-    {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
+        void Start()
         {
-            timeLeft = 0;
+            if (cam == null)
+            {
+                cam = Camera.main;
+            }
+            friendlyObjectList = new List<GameObject>();
+            enemyObjectList = new List<GameObject>();
+            i18n = I18n.Instance;
+
+            StartCoroutine(Spawn());
+            UpdateText();
         }
-        UpdateText();
-        foreach (GameObject friend in friendlyObjectList)
+
+        // Update is called once physics timestamp
+        private void FixedUpdate()
         {
-            Dictionary<GameObject, int> friendAggroMap = friend.GetComponent<Infantry>().AggroMap;
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+            }
+            UpdateText();
+            foreach (GameObject friend in friendlyObjectList)
+            {
+                Dictionary<GameObject, int> friendAggroMap = friend.GetComponent<Infantry>().AggroMap;
+                foreach (GameObject enemy in enemyObjectList)
+                {
+                    Dictionary<GameObject, int> enemyAggroMap = enemy.GetComponent<Infantry>().AggroMap;
+                    if (!friendAggroMap.ContainsKey(enemy))
+                    {
+                        int initAggro = Mathf.RoundToInt(Mathf.Abs((enemy.gameObject.transform.position - friend.gameObject.transform.position).magnitude));
+                        friendAggroMap[enemy] = initAggro;
+                        enemyAggroMap[friend] = initAggro;
+                    }
+                }
+            }
+            foreach (GameObject friend in friendlyObjectList)
+            {
+                Infantry friendInfantry = friend.GetComponent<Infantry>();
+                friendInfantry.targetEnemy = friendInfantry.AggroMap.FirstOrDefault(x => x.Value == friendInfantry.AggroMap.Values.Max()).Key;
+            }
             foreach (GameObject enemy in enemyObjectList)
             {
-                Dictionary<GameObject, int> enemyAggroMap = enemy.GetComponent<Infantry>().AggroMap;
-                if (!friendAggroMap.ContainsKey(enemy))
+                Infantry enemyInfantry = enemy.GetComponent<Infantry>();
+                enemyInfantry.targetEnemy = enemyInfantry.AggroMap.FirstOrDefault(x => x.Value == enemyInfantry.AggroMap.Values.Max()).Key;
+            }
+        }
+
+        private void UpdateText()
+        {
+                timerText.text = i18n.__("TimeText") + Mathf.RoundToInt(timeLeft);
+        }
+
+
+        Infantry SpawnSingleCreep(Vector3 spawnPosition, int teamId)
+        {
+            GameObject newInfantryObj = Instantiate(infantry, spawnPosition, Quaternion.identity);
+            Infantry newInfantry = newInfantryObj.GetComponent<Infantry>() as Infantry;
+            newInfantry.TeamId = teamId;
+            return newInfantry;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+                if (hit.collider != null)
                 {
-                    int initAggro = Mathf.RoundToInt(Mathf.Abs((enemy.gameObject.transform.position - friend.gameObject.transform.position).magnitude));
-                    friendAggroMap[enemy] = initAggro;
-                    enemyAggroMap[friend] = initAggro;
+                    player.targetEnemy = hit.transform.gameObject;
+                }
+                else
+                {
+                    player.targetEnemy = null;
+                    player.targetPosition = mousePos;
                 }
             }
         }
-        foreach (GameObject friend in friendlyObjectList)
+
+        IEnumerator Spawn()
         {
-            Infantry friendInfantry = friend.GetComponent<Infantry>();
-            friendInfantry.targetEnemy = friendInfantry.AggroMap.FirstOrDefault(x => x.Value == friendInfantry.AggroMap.Values.Max()).Key;
-        }
-        foreach (GameObject enemy in enemyObjectList)
-        {
-            Infantry enemyInfantry = enemy.GetComponent<Infantry>();
-            enemyInfantry.targetEnemy = enemyInfantry.AggroMap.FirstOrDefault(x => x.Value == enemyInfantry.AggroMap.Values.Max()).Key;
-        }
-    }
-
-    private void UpdateText()
-    {
-        timerText.text = "Time Left:\n" + Mathf.RoundToInt(timeLeft);
-    }
-
-
-    Infantry SpawnSingleCreep(Vector3 spawnPosition, int teamId)
-    {
-        GameObject newInfantryObj = Instantiate(infantry, spawnPosition, Quaternion.identity);
-        Infantry newInfantry = newInfantryObj.GetComponent<Infantry>() as Infantry;
-        newInfantry.TeamId = teamId;
-        return newInfantry;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-            if (hit.collider != null)
+            for (int i = 0; i < numOfSpawnCreep; i++)
             {
-                player.targetEnemy = hit.transform.gameObject;
+                //Spawn Friendly Creep
+                Vector2 pos = new Vector2(Infantry.xLimit, Random.Range(-Infantry.yLimit, Infantry.yLimit));
+                friendlyObjectList.Add(SpawnSingleCreep(pos, 1).gameObject);
+                //Spawn Enemy Creep
+                pos = new Vector2(-Infantry.xLimit, Random.Range(-Infantry.yLimit, Infantry.yLimit));
+                enemyObjectList.Add(SpawnSingleCreep(pos, 2).gameObject);
             }
-            else
-            {
-                player.targetEnemy = null;
-                player.targetPosition = mousePos;
-            }
+            yield return new WaitForSeconds(30);
         }
     }
-
-    IEnumerator Spawn()
-    {
-        for (int i = 0; i < numOfSpawnCreep; i++)
-        {
-            //Spawn Friendly Creep
-            Vector2 pos = new Vector2(Infantry.xLimit, Random.Range(-Infantry.yLimit, Infantry.yLimit));
-            friendlyObjectList.Add(SpawnSingleCreep(pos, 1).gameObject);
-            //Spawn Enemy Creep
-            pos = new Vector2(-Infantry.xLimit, Random.Range(-Infantry.yLimit, Infantry.yLimit));
-            enemyObjectList.Add(SpawnSingleCreep(pos, 2).gameObject);
-        }
-        yield return new WaitForSeconds(30);
-    }
-
 }
--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using LastHitPractice;
+
 public class PhysicsObject : MonoBehaviour
 {
 
